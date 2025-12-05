bddfe212f4439d746356317ce654e1a69e7ed6de
Andrew Cruz <andrew.cruz@mail.citytech.cuny.edu>
2025-12-05 15:28:16 -0500

    finished

--- lht_calendar.js
+++ lht_calendar.js
@@ -31,7 +31,7 @@
 */
 
 /* Set the date displayed in the calendar */
-let thisDay = new Date("September 24, 2021");
+let thisDay = new Date();
 
 /* Write the calendar to the element with the id "calendar" */
 document.getElementById("calendar").innerHTML = createCalendar(thisDay);
@@ -41,6 +41,7 @@ function createCalendar(calDate) {
     let calendarHTML = "<table id='calendar_table'>";
     calendarHTML += calCaption(calDate);
     calendarHTML += calWeekdayRow();
+    calendarHTML += calDays(calDate);
     calendarHTML += "</table>";
     return calendarHTML;
 }
@@ -64,4 +65,49 @@ function calWeekdayRow() {
 
     rowHTML += "</tr>";
     return rowHTML;
+}
+
+/* Function to calculate the number of days in the month */
+function daysInMonth(calDate) {
+   let dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+   let thisYear = calDate.getFullYear();
+   let thisMonth = calDate.getMonth();
+
+   if (thisYear % 4 === 0) {
+      if ((thisYear % 100 != 0) || (thisYear % 400 === 0)) {
+         dayCount[1] = 29;
+      }
+   }
+
+   return dayCount[thisMonth];
+}
+
+/* Function to write table rows for each day of the month */
+function calDays(calDate) {
+   let day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+   let weekDay = day.getDay();
+
+   let htmlCode = "<tr>";
+   for (let i = 0; i < weekDay; i++) {
+      htmlCode += "<td></td>";
+   }
+
+   let totalDays = daysInMonth(calDate);
+
+   let highlightDay = calDate.getDate();
+   for (let i = 1; i <= totalDays; i++) {
+      day.setDate(i);
+      weekDay = day.getDay();
+
+      if (weekDay === 0) htmlCode += "<tr>";
+      if (i === highlightDay) {
+         htmlCode += "<td class='calendar_dates' id='calendar_today'>" + i + dayEvent[i] + "</td>";
+      } else {
+         htmlCode += "<td class='calendar_dates'>" + i + dayEvent[i] + "</td>";
+      }
+      if (weekDay === 6) htmlCode += "</tr>";
+   }
+
+   return htmlCode;
 }
